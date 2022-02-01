5dd29f1ccaa69beefe0ac369293cb6bb357e6dc6
Stéphanie AMANS <stephanie-amans_student2021@wilder.school>
2022-02-01 21:38:29 +0100

    end of the HomePage

--- assets/app.js
+++ assets/app.js
@@ -11,4 +11,6 @@ import './styles/app.scss';
 // start the Stimulus application
 import './bootstrap';
 
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all');
 require('bootstrap');
