cf5c335930b87ffeed814c8a03988b59bb93a616
Stéphanie AMANS <stephanie-amans_student2021@wilder.school>
2022-02-03 12:23:04 +0100

    create page all trainings

--- src/DataFixtures/TrainingFixtures.php
+++ src/DataFixtures/TrainingFixtures.php
@@ -13,28 +13,36 @@ class TrainingFixtures extends Fixture implements DependentFixtureInterface
     {
         $training = new Training();
         $training->setName('Fly Yoga');
+        $this->addReference('cours1', $training);
         $training->setDescription('Yoga entre Ciel et Terre - postions de yoga dans un hamac et 
         étirements sur tapis');
         $training->addEquipment($this->getReference('hamac'));
+        $training->setUrl('https://64.media.tumblr.com/299ffb7ee53348ffd99eb2fe58592571/c960106fc36c08cb-8e/s1280x1920/979fd121c97e4d5b9be5d057544b970557979bb3.jpg');
         $manager->persist($training);
 
         $training2 = new Training();
         $training2->setName('Fly méditation');
+        $this->addReference('cours2', $training);
         $training2->setDescription('Yoga entre Ciel et Terre - méditation dans un hamac et 
         réveil sur tapis');
         $training2->addEquipment($this->getReference('hamac'));
+        $training2->setUrl('https://i.pinimg.com/originals/4a/de/67/4ade67f82dd06305fda1307c47bdf4e4.jpg');
         $manager->persist($training2);
 
         $training3 = new Training();
         $training3->setName('Yoga Yin Yang');
+        $this->addReference('cours3', $training);
         $training3->setDescription('Yoga sur tapis - étirements et respiration');
         $training3->addEquipment($this->getReference('tapis'));
+        $training3->setUrl('https://www.casayoga.tv/images/images-videos/564-casayogatv-cours-en-ligne-yin-yoga-4-5-posture-du-chat-demi-selle-torsion-surelevee-delphine-martin-michaud-yin-yoga-6387.jpg');
         $manager->persist($training3);
 
         $training4 = new Training();
         $training4->setName('Massage bien-être');
+        $this->addReference('cours4', $training);
         $training4->setDescription('Massage au choix - selon votre envie et votre humeur du moment');
         $training4->addEquipment($this->getReference('table de massage'));
+        $training3->setUrl('https://www.experiencecotedazur.com/wp-content/uploads/wpetourisme/big-ticket-image-60787716c57b4845225298-cropped600-400.jpeg');
         $manager->persist($training4);
 
         $manager->flush();
@@ -43,7 +51,6 @@ class TrainingFixtures extends Fixture implements DependentFixtureInterface
     public function getDependencies()
     {
         return [
-            UserFixtures::class,
             EquipmentFixtures::class
         ];
     }
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,10 +4,11 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class UserFixtures extends Fixture
+class UserFixtures extends Fixture implements DependentFixtureInterface
 {
     private UserPasswordHasherInterface $passwordHasher;
 
@@ -53,7 +54,8 @@ class UserFixtures extends Fixture
 
         $user4 = new User();
         $user4->setName('Audrey');
-        $this->addReference('Audrey', $user4);
+        $this->addReference('audrey', $user4);
+        $user4->addTraining($this->getReference('cours1'));
         $user4->setEmail('audrey.martin@teacher.fr');
         $user4->setUrl('https://static.wixstatic.com/media/a73a52_c8c1df3f431847dbb5cfab6fa3a05c97~mv2.jpg/v1/crop/x_0,y_180,w_720,h_720/fill/w_220,h_220,al_c,q_80,usm_0.66_1.00_0.01/Resized_StudioEquilibre_22_07_20_BD__ANA.webp');
         $user4->setDescription('Ancienne danseuse et passionnée de yoga, je me suis formée au yoga aérien et à la méditation');
@@ -68,6 +70,7 @@ class UserFixtures extends Fixture
         $user5 = new User();
         $user5->setName('Anaïs');
         $this->addReference('Anaïs', $user5);
+        $user5->addTraining($this->getReference('cours3'));
         $user5->setEmail('anais.hattier@teacher.fr');
         $user5->setUrl('https://www.yogabikrambordeaux.com/wp-content/uploads/2021/10/Bikram.Bx03.06.21.BD%E2%94%AC%C2%AEANAKA-112-e1634915538817.jpg');
         $user5->setDescription('Je propose du Yoga Yin & Yang');
@@ -82,6 +85,7 @@ class UserFixtures extends Fixture
         $user6 = new User();
         $user6->setName('Stéphane');
         $this->addReference('Stéphane', $user6);
+        $user4->addTraining($this->getReference('cours4'));
         $user6->setEmail('stephane.lupin@teacher.fr');
         $user6->setUrl('https://media.aladom.fr/users/999821/m/0.jpg');
         $user6->setDescription('Passionné de massages depuis tout jeune, je vous propose un instant bien-être au sein de cette belle équipe');
@@ -94,5 +98,12 @@ class UserFixtures extends Fixture
         $manager->persist($user6);
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            TrainingFixtures::class
+        ];
     }
 }
