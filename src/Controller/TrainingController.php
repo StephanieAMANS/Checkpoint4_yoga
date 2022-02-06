8161e1fc5cd3575f14029d7f1a35a8410f5f0670
Stéphanie AMANS <stephanie-amans_student2021@wilder.school>
2022-02-06 17:26:49 +0100

    create Training form

--- src/Controller/TrainingController.php
+++ src/Controller/TrainingController.php
@@ -6,6 +6,7 @@ use App\Entity\Training;
 use App\Repository\TrainingRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,6 +22,24 @@ class TrainingController extends AbstractController
         return $this->render('training/index.html.twig', [
             'trainings' => $trainings,
         ]);
+    }
+    /**
+     * @Route("/training/add", name="training_add")
+     */
+    public function create(Request $request, ManagerRegistry $managerRegistry): Response
+    {
+        $training = new Training;
+        $form = $this->createForm(TrainingPurposeType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $managerRegistry->getManager();
+            $entityManager->persist($training);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le nouveau cours a bien été enregistré');
+            return $this->redirectToRoute('home');
+        }
+        return $this->renderForm('training/add.html.twig');
     }
 
     /**
